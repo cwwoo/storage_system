0097f279d99f57189168934a05f0e3141d295b25
cwwoo <cwwoo.psm@gmail.com>
2018-03-17 07:21:40 +0000

    complete

--- ch26_t1.c
+++ ch26_t1.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include <pthread.h>
-#include "mythreads.h"
+#include "ch26_mythreads.h"
 
 static volatile int counter = 0;
 
-void *mythread(void *arg){
+void *mythread(void *arg)
+{
 	printf("%s: begin\n", (char *) arg);
 	int i;
-	for (i = 0; i < 1e7; i++){
-		counter = counter + 1;
+	for (i = 0; i < 1e7; i++) {
+	counter = counter + 1;
 	}
 	printf("%s: done\n", (char *) arg);
 	return NULL;
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 	pthread_t p1, p2;
 	printf("main: begin (counter = %d)\n", counter);
-	pthread_create(&p1, NULL, mythread, "A");
-	pthread_create(&p2, NULL, mythread, "B");
-	pthread_join(p1, NULL);
-	pthread_join(p2, NULL);
+	Pthread_create(&p1, NULL, mythread, "A");
+	Pthread_create(&p2, NULL, mythread, "B");
+	Pthread_join(p1, NULL);
+	Pthread_join(p2, NULL);
 	printf("main: done with both (counter = %d)\n", counter);
 	return 0;
 }
-
-
-
